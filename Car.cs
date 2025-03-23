6c1b2253df7790406a4ee5e02ebefb42d0635ccc
Kimberly-git53 <kimalvarez2014@gmail.com>
2025-03-22 23:51:54 -0500

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassLibrary
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public string Color { get; set; }
+        public int Miles { get; set; }
+        public bool IsNew { get; set; }
+        public decimal SizeOfEngine { get; set; }
+
+        public Car(string make,  string model, int year, string color, decimal price = 0, int miles = 0, bool isNew = true, decimal sizeOfEngine = 0)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Price = price;
+            Color = color;
+            Miles = miles;
+            IsNew = isNew;
+            SizeOfEngine = sizeOfEngine;
+        }
+
+        public Car() 
+        {
+            Make = "Nothing yet";
+            Model = "Nothing yet";
+            Year = 0; 
+            Price = 0;
+            Color = "Nothing yet";
+            Miles = 0;
+            IsNew = true;
+            SizeOfEngine = 0;
+        }
+        public override string ToString()
+        {
+            return $"{Year} {Make} {Model} {Color} {Miles} - New car: {IsNew} - Engine size = {SizeOfEngine} - ${Price}";
+        }
+
+    }
+}
---
+++ FileIO.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassLibrary
+{
+    public class FileIO
+    {
+        Store store;
+        
+        // constructor gets a reference to the store. The inventory will be saved to and loaded from a file
+        public FileIO(Store store)
+        { 
+            this.store = store; 
+        }
+
+        //Store.txt file will be created in the same directory as the executable.
+        public void SaveInventory()
+        {
+            string filename = "store.txt";
+
+            // It's a best practice to use "using" statements to ensure that IDisposable objects are disposed of correctly.
+            // stream writer is used to write text to a file.
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                // Write each car to the file. One car per line, with fields separated by commas. 
+                foreach (Car car in store.CarList)
+                {
+                    writer.WriteLine($"{car.Make}, {car.Model}, {car.Year}, {car.Price}, {car.Color}, {car.Miles}, New:{car.IsNew}, Engine size:{car.SizeOfEngine}");
+                }
+            }
+        }
+
+        // Load the store from the file. This method returns a list of cars.
+        public List<Car> LoadStore()
+        {
+            List<Car> cars = new List<Car>();
+            string filename = "store.txt";
+
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    //Split the line into parts using the comma as a separator
+                    string[] parts = line.Split(',');
+                    string make = parts[0];
+                    string model = parts[1];
+                    string color = parts[2];
+                    // Always good to handle potential format issues with int and decimal parsing
+                    try
+                    {
+                        int year = int.Parse(parts[3]);
+                        int miles = int.Parse(parts[4]);
+                        decimal price = decimal.Parse(parts[5]);
+                        decimal sizeOfEngine = decimal.Parse(parts[6]);
+
+                        // Add boolean status
+                        bool isNew = bool.Parse(parts[7]);
+
+                        Car car = new Car(make, model, year, color, price, miles, isNew, sizeOfEngine);
+                        cars.Add(car);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error parsing data for car: " + line);
+                    }
+                }
+            }
+            return cars;
+        }
+    }
+}
